c50b5922ac17f0c223c7c70580c7bb5001858ae5
iamsarb001 <sarbbsandhu555@gmail.com>
2025-03-23 16:21:18 +0530

    landing page- completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 
+import { Avatar } from 'flowbite-react';
 import { ArrowRight, ChevronDown, Eye, FileText, Hash, Lightbulb, PartyPopper, Plus, Search, ThumbsUp, X  } from 'lucide-react';
 
 function App() {
@@ -17,7 +18,7 @@ function App() {
   return (
     <div  className ='bg-[#f4f0e6]'>
 
-        <div className='mx-48'>
+        <div className='mx-48 cursor-pointer'>
 
           <section id = "header" className='grid grid-cols-2 gap-[10px] py-4  items-center '>
 
@@ -61,6 +62,7 @@ function App() {
           </section>
 
           <section id = "mainsection" className='p-2 my-4 grid grid-cols-2 gap-16'>
+
             <div>
               <div className='text-4xl font-bold'> Slack is where the future works </div>
               <div className='text-[15px] my-4'> Transform the way you work with one place for everyone and everything you need to get stuff done </div>
@@ -76,23 +78,94 @@ function App() {
               </div>
             </div>
 
-            <div>
-                <div>
-                  <div> 3 </div> 
-                  <div>  <ChevronDown  /> Channels <Plus  /> </div>
-                  <div> <Hash  /> project-unicorn </div>
-                </div>
-                <div>
-                  <div> #project-unicorn </div>
+            <div className='relative'>
+              
+                <div className='rounded-xl  w-full h-60 grid grid-rows-[1fr_6fr] '>
+
                   <div>
-                        <ThumbsUp  />
-                        <Lightbulb />
-                        <PartyPopper  />
-                        <FileText  />
-                        <Eye  />
+                      <div className='bg-[#43126a] flex  gap-1 w-full h-full rounded-t-[10px] pl-2 pt-2 '>
+                        <div className='bg-pink-900 rounded-xl w-[12px] h-[2vh]'>  </div>
+                        <div className='bg-yellow-500 rounded-xl w-[12px] h-[2vh]'>  </div>
+                        <div  className='bg-green-700 rounded-xl w-[12px] h-[2vh]'>  </div>
+                      </div>
+                  </div>
+
+                  <div className=' grid grid-cols-[2fr_5fr]'>
+                      
+                      <div className='bg-[#944c9e] text-xs  text-white font-bold rounded-bl-[10px] '>
+
+                        <div className='my-4'>
+
+                          <div className='grid grid-cols-[0.5fr_2fr_0.5fr] py-1 gap-2 items-center px-1 '>  
+                            <div> <ChevronDown  size={14} /> </div>  
+                            <div> Channels </div>
+                            <div> <Plus  size={14} /> </div>  
+                          </div>
+
+                          <div className='grid grid-cols-[1fr_5fr] gap-1 px-1 bg-[#2a74d4] p-1 '> 
+                            <div> <Hash size  = {15}  />  </div> 
+                            <div className='text-[10px] flex items-center '>  project-unicorn  </div>
+                            </div>
+                        </div>
+
+                      </div>
+
+                      <div className='bg-white rounded-br-[10px] px-6 py-2 '>
+
+                        <div className='grid grid-cols-2 gap-4'>
+                          <div className='text-[13px] font-bold flex items-center'>  #project-unicorn </div>
+                          <div>  
+
+                          <Avatar.Group>
+                              <Avatar img="/men-1.webp"  stacked />
+                              <Avatar img="/men2.webp"  stacked />
+                              <Avatar img="/men-3.webp"  stacked />
+                              <Avatar img="/men-4.jpg"  stacked />
+                              <Avatar img="/men-5.webp"  stacked />
+                            </Avatar.Group>
+                          </div>
+                        </div>
+
+                        <div>
+                          
+                            <div className='grid grid-cols-[1fr_5fr] gap-1'>
+                                <div> 
+                                  <Avatar img = "white-female.jpg" bordered />
+                                </div>
+                                <div className='text-[10px]'>
+                                  <div className='text-[11px] font-bold'> Zoe Maxwell </div>
+                                  <div> Are we ready for launch ? </div>
+                                </div>
+                            </div>
+
+                            <div className='grid grid-cols-[1fr_5fr] gap-1 py-2'>
+                                <div> 
+                                    <Avatar img = "male-img.jpg" bordered />
+                                </div>
+                                <div className='text-[10px]'>
+                                  <div className='text-[11px] font-bold'> Matt Brewer </div>
+                                  <div> Here's the run of show: </div>
+                                </div>
+                            </div>
+
+                            <div className='grid ml-12 grid-cols-[0.5fr_5fr] w-40  gap-1 p-1 border-2 border-gray-300 rounded-[5px] '>
+                              <div>  <FileText /> </div>
+                              <div className='grid grid-rows-2 gap-[1px] py-1'>  
+                                <div className='bg-gray-200 w-22 h-[5px] rounded-2xl '>  </div>
+                                <div className='bg-gray-200 w-13 h-[5px] rounded-2xl '>  </div>
+                              </div>
+                            </div>
+
+                        </div>
+
+                      
+                      </div>
+                    
                   </div>
                 </div>
+                
             </div>
+
           </section>
 
           <section id = "logo-section">
@@ -109,8 +182,8 @@ function App() {
           
           <section id = "future-section" className='grid grid-cols-2 gap-2'>
 
-            <div className='w-full'>  
-              <img src = "/future-img.PNG"  alt = "future" />
+            <div className='w-full h-[30vh]'>  
+              <img src = "/future-img.PNG" className='h-[25vh]'  alt = "future" />
             </div>
 
             <div>
@@ -123,22 +196,17 @@ function App() {
 
           </section>
 
-          <section id = "toast-section">
-            <div> This website uses cookies to enhane user experience and to analyze performance and traffic on our website . We also share information about your use of our site with our social media, adverstising and analytic partners <span> More Info </span> </div>
-            <div>
-              <button> ACCEPT ALL COOKIES </button>
-            </div>
-            <div> Hey there /  Want to know something cool ? </div>
-            <div>
-              hand sign
-            </div>
-            <div> /\ </div>
-          </section>
+
+          <div section = "emoji-section">
+              <button  className='bg-gray-100 p-1 px-2 rounded-2xl absolute top-32 right-115 '>  👍 3  </button>
+              <button  className='bg-gray-100 p-1 px-2 rounded-2xl absolute top-60 right-45 '>  💡  8  </button>
+              <button  className='bg-gray-100 p-1 px-2 rounded-2xl absolute top-65 right-142 '>  🎉 5  </button>
+              <button  className='bg-gray-100 p-1 px-2 rounded-2xl absolute top-85 right-60 '> 👀 5   </button>
+          </div>
 
         </div>
         
     </div>
   )
 }
-
 export default App
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import flowbite from 'flowbite-react/tailwind'
 
 // https://vite.dev/config/
 export default defineConfig({
+  content : [
+    flowbite.content()
+  ],
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
+    flowbite.plugin()
   ],
 })
